ccca1fa58b0adb33ca49eeef3af7225a5b1c5396
Pranav Ghosh <pranavghosh19@gmail.com>
2025-07-09 17:59:18 +0000

    make the table #F1F5F9 and while hovering #FFDE66

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -58,7 +58,7 @@ const TableRow = React.forwardRef<
   <tr
     ref={ref}
     className={cn(
-      "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
+      "border-b transition-colors bg-secondary hover:bg-accent hover:text-accent-foreground data-[state=selected]:bg-muted",
       className
     )}
     {...props}

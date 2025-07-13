fef9d8847c474da459676622fc4106d606724088
Pranav Ghosh <pranavghosh19@gmail.com>
2025-07-13 04:59:45 +0000

    shadow effect on the bottom nav bar

--- src/components/bottom-bar.tsx
+++ src/components/bottom-bar.tsx
@@ -24,7 +24,7 @@ export function BottomBar() {
   }
 
   return (
-    <div className="sm:hidden fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
+    <div className="sm:hidden fixed bottom-0 left-0 z-50 w-full h-16 bg-background border-t shadow-[0_-1px_2px_0_rgba(0,0,0,0.05)]">
       <div className="grid h-full max-w-lg grid-cols-2 mx-auto font-medium">
         <Button variant="ghost" asChild className="h-full rounded-none">
           <Link href="/login">Log In</Link>
